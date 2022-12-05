7b6b33d870d3c54c33b0889f8989d0796df417a8
agustin <agustinvillagran98@gmail.com>
2022-12-05 11:59:36 -0300

    setting mongo

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
+const Note = require('./models/note')
 const { request } = require('express')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 
+
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
-
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-]
-app.use(express.static('build'))
 
 const generateId = () =>{
   const maxId = notes.length > 0
@@ -37,7 +18,9 @@ const generateId = () =>{
 
 
 app.get('/api/notes',(request,response) =>{
+  Note.find({}).then(notes => {
     response.json(notes)
+  })
 })
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
@@ -81,6 +64,6 @@ app.post('/api/notes',(request, response) =>{
 })
 
 
-const PORT = 3001
+
 app.listen(PORT,() =>{
 console.log(`Server running on port ${PORT}`)})
