d72d0a3d38298e68783547e40282c49e7e973b91
agustin <agustinvillagran98@gmail.com>
2022-12-09 11:50:53 -0300

    eslint installed

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+	'env':
+	{
+		'browser': true,
+		'commonjs': true,
+		'es2021': true
+	},
+	'extends': 'airbnb',
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'no-tabs':0,
+		'eqeqeq': 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': [
+			'error', 'always'
+		],
+		'object-curly-newline':[ 
+			'error', { 'ObjectPattern': { 'multiline': true }}
+		],
+		'no-underscore-dangle': 0,
+		'arrow-spacing': [
+			'error', { 'before': true, 'after': true }
+		],
+		'no-console': 0,
+		'linebreak-style': [
+			'error', 'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
