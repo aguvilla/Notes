8ec8e85340ea217269999ab6a38adcf33acf0bf5
agustin <agustinvillagran98@gmail.com>
2023-03-11 11:27:09 -0300

    a

--- test/note_api.test.js
+++ test/note_api.test.js
@@ -3,13 +3,14 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
-afterAll(async () => {
-	mongoose.connection.close()
-})
 
 test('notes are returned as json', async () => {
 	await api
 		.get('/api/notes')
 		.expect(200)
 		.expect('Content-Type', /json/)
+})
+
+afterAll(() => {
+	mongoose.connection.close()
 })
